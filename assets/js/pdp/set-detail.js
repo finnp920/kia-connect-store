e6661c8c55058aa2797c455c079f2825c6dfed28
finnp920 <99485859+finnp920@users.noreply.github.com>
2025-11-23 02:21:28 +0900

    feat: 일부 이미지 업데이트 및 FIFA 페이지 작업

--- assets/js/pdp/set-detail.js
+++ assets/js/pdp/set-detail.js
@@ -7,6 +7,7 @@
 // html
 const themeList = _themeList || ['01', '02', '03', '04', '05'];
 let currentTheme = _currentTheme || '01';
+let isLiked = false;
 
 // ----------------------------------------
 // Initialization
@@ -16,21 +17,86 @@ document.addEventListener('DOMContentLoaded', async () => {
     const urlTheme = getQueryParam('theme');
     setCurrentTheme(urlTheme);
 
+    setupEventListeners();
     initSwiper();
   } catch (e) {}
 });
 
 function setCurrentTheme(newTheme) {
-  if (newTheme && themeList[newTheme]) {
+  if (newTheme && themeList.includes(newTheme)) {
     currentTheme = newTheme;
   }
 
-  const mainElement = document.getElementsByTagName('main');
-
-  if (!mainElement || !mainElement.length) {
+  const mainElements = document.getElementsByTagName('main');
+  if (!mainElements || !mainElements.length) {
     return;
   }
-  mainElement[0].dataset.currentTheme = currentTheme;
+
+  mainElements[0].dataset.currentTheme = currentTheme;
+}
+
+// ----------------------------------------
+// Event Binding
+// ----------------------------------------
+function setupEventListeners() {
+  document.querySelectorAll('.option-item').forEach((item) => {
+    item.addEventListener('click', () => setCurrentTheme(item.dataset.theme));
+  });
+
+  document.querySelectorAll('.themes-cards-grid .card').forEach((selector) => {
+    selector.addEventListener('click', () =>
+      setCurrentTheme(selector.dataset.theme)
+    );
+  });
+
+  // PC용 테마 선택 버튼
+  document.querySelectorAll('.theme-selector button').forEach((selector) => {
+    selector.addEventListener('click', () =>
+      setCurrentTheme(selector.dataset.theme)
+    );
+  });
+
+  // MO용 테마 선택 Dropdown
+  const dropdownBox = document.querySelector('.theme-selector.dropdown');
+  const dropdownButton = document.getElementById('theme-selector-dropdown');
+  const dropdownList = document.querySelector(
+    '.theme-selector.dropdown .theme-list'
+  );
+  if (dropdownButton && dropdownList && dropdownBox) {
+    // 셀렉트 버튼 클릭 시 Dropdown 박스 active 토글
+    dropdownButton.addEventListener('click', (e) => {
+      e.stopPropagation();
+      dropdownBox.classList.toggle('active');
+    });
+
+    // 리스트 항목 클릭 시
+    const themeItems = dropdownList.querySelectorAll('li');
+    themeItems.forEach((item) => {
+      item.addEventListener('click', () => {
+        const themeValue = item.getAttribute('value');
+        dropdownButton.textContent = item.textContent;
+        dropdownBox.classList.remove('active');
+        if (dropdownBox) {
+          dropdownBox.setAttribute('data-theme', themeValue);
+        }
+        setCurrentTheme(themeValue);
+      });
+    });
+  }
+  // 외부 클릭 시 Dropdown 닫기
+  document.addEventListener('click', (e) => {
+    if (dropdownBox && !e.target.closest('.theme-selector.dropdown')) {
+      dropdownBox.classList.remove('active');
+    }
+  });
+
+  // 좋아요 버튼
+  const btnLike = document.getElementById('btnLike');
+  if (btnLike) btnLike.addEventListener('click', handleLikeClick);
+
+  // 구매 버튼
+  const btnBuy = document.getElementById('btnBuy');
+  if (btnBuy) btnBuy.addEventListener('click', handleBuyClick);
 }
 
 // ----------------------------------------
@@ -61,4 +127,17 @@ function initSwiper() {
 function getQueryParam(param) {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(param);
+}
+
+function handleLikeClick() {
+  const btnLike = document.getElementById('btnLike');
+  if (!btnLike) return;
+
+  isLiked = !isLiked;
+  btnLike.classList.toggle('active', isLiked);
+}
+
+function handleBuyClick() {
+  console.log('Buy');
+  alert(`Proceeding to checkout\nTheme: ${currentTheme}`);
 }
